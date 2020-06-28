b61ef637ae33c09d2990298556d337ba7183e3ad
unknown <stvort@mail.ru>
2020-06-29 01:15:43 +0400

    L24-di added

---
+++ L24-di/homework-template/src/main/java/ru/otus/App.java
@@ -0,0 +1,23 @@
+package ru.otus;
+
+import ru.otus.appcontainer.AppComponentsContainerImpl;
+import ru.otus.appcontainer.api.AppComponentsContainer;
+import ru.otus.config.AppConfig;
+import ru.otus.services.GameProcessor;
+
+/*
+В классе AppComponentsContainerImpl реализовать обработку, полученной в конструкторе конфигурации,
+основываясь на разметке аннотациями из пакета appcontainer. Так же необходимо реализовать методы getAppComponent.
+В итоге должно получиться работающее приложение. Менять можно только класс AppComponentsContainerImpl.
+
+PS Приложение представляет из себя тренажер таблицы умножения)
+*/
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig.class);
+        GameProcessor gameProcessor = container.getAppComponent(GameProcessor.class);
+        gameProcessor.startGame();
+    }
+}
