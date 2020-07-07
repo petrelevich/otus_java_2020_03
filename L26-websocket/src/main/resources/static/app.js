9d4da0042bda22b9afd5166655bbe6d3140582ba
Sergey Petrelevich <petrelevich@yandex.ru>
2020-07-08 00:55:45 +0300

    L26 webSockets

---
+++ L26-websocket/src/main/resources/static/app.js
@@ -0,0 +1,44 @@
+let stompClient = null;
+
+const setConnected = (connected) => {
+  $("#connect").prop("disabled", connected);
+  $("#disconnect").prop("disabled", !connected);
+  if (connected) {
+    $("#chatLine").show();
+  } else {
+    $("#chatLine").hide();
+  }
+  $("#message").html("");
+}
+
+const connect = () => {
+  stompClient = Stomp.over(new SockJS('/gs-guide-websocket'));
+  stompClient.connect({}, (frame) => {
+    setConnected(true);
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/topic/response.' + $("#roomId").val(), (greeting) => showGreeting(JSON.parse(greeting.body).messageStr));
+  });
+}
+
+const disconnect = () => {
+  if (stompClient !== null) {
+    stompClient.disconnect();
+  }
+  setConnected(false);
+  console.log("Disconnected");
+}
+
+const sendMsg = () => stompClient.send("/app/message." + $("#roomId").val(), {}, JSON.stringify({'messageStr': $("#message").val()}))
+
+
+const showGreeting = (messageStr) => $("#chatLine").append("<tr><td>" + messageStr + "</td></tr>")
+
+
+$(function () {
+  $("form").on('submit', (event) => {
+    event.preventDefault();
+  });
+  $("#connect").click(connect);
+  $("#disconnect").click(disconnect);
+  $("#send").click(sendMsg);
+});
